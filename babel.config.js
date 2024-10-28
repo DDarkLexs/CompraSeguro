c39dccb35f3314cfe0ad1825e0f1aac5fefc6aad
DDark <antoniolexis2001@outlook.com>
2024-10-28 17:24:27 +0100

    implementação do react native paper 🛠✨

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo',"module:@expo/knex-expo-sqlite-dialect/babel-preset"],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,22 @@
  * @format
  */
 
-import { SafeAreaView, Text, useColorScheme } from "react-native";
-import {Provider as StoreProvider} from 'react-redux';
-import { store } from './store/index';
+import { SafeAreaView, useColorScheme } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
 import AppNavigation from "./navigation";
-import { useEffect, useState } from "react";
+import { store } from "./store/index";
 
 function App(): JSX.Element {
-  const theme = useColorScheme() == 'light' ? 'light' : 'dark';
+  const theme = useColorScheme() == "light" ? "light" : "dark";
   return (
+    <PaperProvider>
       <StoreProvider store={store}>
-          <SafeAreaView style={{flex: 1}}>
-              <AppNavigation />
-          </SafeAreaView>
+        <SafeAreaView style={{ flex: 1 }}>
+          <AppNavigation />
+        </SafeAreaView>
       </StoreProvider>
+    </PaperProvider>
   );
 }
 
