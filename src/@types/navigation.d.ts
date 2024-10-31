fcb68d2203f46480e9b1a09b27bb8f3e437307de
DDark <antoniolexis2001@outlook.com>
2024-10-31 17:01:11 +0100

    configurado a typagem do navigation 🛠✨

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,7 @@
+const {Routes} = Modules;
+declare type StackScreen = {
+  [Routes.MAIN]: undefined;
+  [Routes.PRODUCTS]: undefined;
+  [Routes.TASKS]: undefined;
+  [Routes.HOME]: undefined;
+};
--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,6 +1,11 @@
 export enum Routes {
     // LOGIN = 'ENTRADA',
     // REGISTER = 'REGISTRO',
+    MAIN = 'MAIN',
+    HOME = 'PRINCIPAL',
+    TASKS = 'COMPRAS',
+    PRODUCTS = 'PRODUTOS',
+    
 
 }
   
