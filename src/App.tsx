3c11e4453edc68a30b7630787e6e63e95305d243
DDark <antoniolexis2001@outlook.com>
2025-01-23 02:27:31 +0100

    ajustado o safeArea error ✨🛠️

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,59 @@
-import React, { useEffect, useState } from "react";
-import { Platform, SafeAreaView, useColorScheme, View } from "react-native";
-import { PaperProvider } from "react-native-paper";
-import { Provider as StoreProvider } from "react-redux";
-import AppNavigation from "./navigation";
-import { store } from "./store/index";
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { dark, light } from "./styles/theme";
-import {createSchema, dropTablesAndTriggers}  from "./database/Schema";
-  
-  SplashScreen.preventAutoHideAsync(); // Previne a splash screen de fechar automaticamente
+import React, {useEffect, useState} from 'react';
+import {useColorScheme, View} from 'react-native';
+import 'react-native-gesture-handler';
+import {PaperProvider} from 'react-native-paper';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Provider as StoreProvider} from 'react-redux';
+import {createSchema} from './database/Schema';
+import AppNavigation from './navigation';
+import {store} from './store/index';
+import {dark, light} from './styles/theme';
 
+SplashScreen.preventAutoHideAsync(); // Previne a splash screen de fechar automaticamente
 
 function App(): JSX.Element {
-   const theme = useColorScheme() == 'light' ?  light : dark;
+    const theme = useColorScheme() == 'light' ? light : dark;
 
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+    const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  const loadFonts = async () => {
-    await Font.loadAsync({
-      'Nunito-Bold': require('../assets/fonts/Nunito-Bold.ttf'),
-      'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
-      'Nunito-light': require('../assets/fonts/Nunito-Light.ttf'),
-      'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
-    });
-    setFontsLoaded(true);
-  
-      await SplashScreen.hideAsync(); // Oculta a splash screen após o carregamento das fontes
-    
-  };
+    const loadFonts = async () => {
+        await Font.loadAsync({
+            'Nunito-Bold': require('../assets/fonts/Nunito-Bold.ttf'),
+            'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
+            'Nunito-light': require('../assets/fonts/Nunito-Light.ttf'),
+            'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
+        });
+        setFontsLoaded(true);
 
-  useEffect(() => {
-    loadFonts();
-    createSchema();
-    // dropTablesAndTriggers();
-  }, []);
+        await SplashScreen.hideAsync(); // Oculta a splash screen após o carregamento das fontes
+    };
 
-  if (!fontsLoaded) {
-    return <></>; // Retorna null enquanto as fontes carregam
-  }
+    useEffect(() => {
+        loadFonts();
+        createSchema();
+        // dropTablesAndTriggers();
+    }, []);
 
-  return (
-    <PaperProvider theme={theme}>
-      <StoreProvider store={store}>
-        <View style={{ flex: 1 }}>
-          <AppNavigation />
-        </View>
-      </StoreProvider>
-    </PaperProvider>
-  );
+    if (!fontsLoaded) {
+        return <></>; // Retorna null enquanto as fontes carregam
+    }
+
+    return (
+        <SafeAreaProvider>
+            <PaperProvider theme={theme}>
+                <StoreProvider store={store}>
+                    <View style={{flex: 1}}>
+                        <AppNavigation />
+                    </View>
+                </StoreProvider>
+            </PaperProvider>
+        </SafeAreaProvider>
+    );
 }
 
 export default App;
-
 
 /* 
 npx expo run:android --variant release
